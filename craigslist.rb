73fc684fde80bf5410ca09b7772b1842d764b710
Alexander Cheung <alex.y.cheungster@gmail.com>
2016-03-17 22:18:23 -0700

    make craigslist script interactive

--- craigslist.rb
+++ craigslist.rb
@@ -1,33 +1,66 @@
+# SELLER INFO
+puts "email: "
+email = gets.gsub("\n","")
+
+puts "full name: "
+name = gets.gsub("\n","")
+
+puts "zip code: "
+zip_code = gets.gsub("\n","")
+
+# TICKET INFO
+puts "artist: "
+artist = gets.gsub("\n","")
+
+puts "event location: "
+location = gets.gsub("\n","")
+
+puts "event date: "
+date = gets
+
+puts "# of tickets: "
+number_of_tickets = gets.gsub("\n","")
+
+puts "price per ticket: "
+price_per_ticket = gets.gsub("\n","")
+
+title = artist + " - " + location + " - " + date + " - " + number_of_tickets + " tickets - $" + price_per_ticket
+body = "Selling " + number_of_tickets + " " + artist + " tickets ($" + price_per_ticket + "/ticket) at " + location + " on " + date + ". Accepting Paypal or Venmo!"
+
 require 'watir-webdriver'
 b = Watir::Browser.new
 b.goto("https://post.craigslist.org/c/sfo")
-sleep(rand(3..5))
-b.radios[5].set
-sleep(rand(5..7))
+
+b.radio(value: "fso").set
+
 b.radio(value: "44").set
-sleep(rand(3..5))
+
 b.radio(value: "1").set
-sleep(rand(3..9))
+
 # TODO: use all radios instead of just first ten
-b.radios[rand(0..10)].set
-b.text_field(name: "FromEMail").set("xxx@gmail.com")
-b.text_field(name: "ConfirmEMail").set("xxx@gmail.com")
-b.checkbox(name: "contact_text_ok").set("1")
-b.text_field(name: "contact_phone").set("555-555-5555")
-b.text_field(name: "contact_name").set("Edwin Lee")
-b.text_field(name: "PostingTitle").set("ArtistName Venue Date Number of Tickets")
-b.text_field(name: "Ask").set("69")
-b.text_field(name: "postal").set("94133")
-PBS = ["Suck me","Blow me","Tickle me", "Gross, dude"]
-b.text_field(name: "PostingBody").set(PBS[rand(0..2)])
-sleep(rand(24..32))
-#change sleep time to 240-320
-b.text_field(name: "number_available").set("4")
+b.radios[rand(0..5)].set
+
+b.text_field(name: "FromEMail").set(email)
+b.text_field(name: "ConfirmEMail").set(email)
+
+b.text_field(name: "contact_name").set(name)
+
+b.text_field(name: "PostingTitle").set(title)
+
+b.text_field(name: "Ask").set(price_per_ticket)
+
+b.text_field(name: "postal").set(zip_code)
+
+b.text_field(name: "PostingBody").set(body)
+
+b.text_field(name: "number_available").set(number_of_tickets)
+
 b.button(name: "go").click
-sleep(rand(2..3))
+
 b.button(text: "continue").click
+
 b.button(text: "done with images").click
-sleep(rand(2..3))
+
 b.button(text: "publish").click
-sleep(rand(2..3))
+
 b.button(name: "go").click
