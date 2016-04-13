9058cc557de79670f1d77251b81997dcf2d79f1a
Alexander Cheung <alex.y.cheungster@gmail.com>
2016-04-13 09:52:19 -0700

    print instead of puts to prompt user to type values in

--- ticketmaster_craigslist_poster.rb
+++ ticketmaster_craigslist_poster.rb
@@ -1,35 +1,37 @@
-# CREATE CGLIST ADD WITH LATEST TICKETMASTER EVENT
+# CREATE CGLIST AD WITH LATEST TICKETMASTER EVENT
 
 # TODO: 
 # - add seats to message body
 # - spin text so craigslist thinks ads are unique (jordan /  mel already started this)
 # - file with secret values to pull all variables from (ie, ticketmaster_email)
+# - store ads in database
+# - cron job to post cglist ad as soon as old one expires
 
 require 'watir-webdriver'
 
-puts "your name: "
+print "your name: "
 name = gets.gsub("\n", "")
-puts "zip code: "
+print "zip code: "
 zip_code = gets.gsub("\n", "")
-puts "ticketmaster email: "
+print "ticketmaster email: "
 ticketmaster_email = gets.gsub("\n", "")
-puts "ticketmaster password: "
+print "ticketmaster password: "
 ticketmaster_password = gets.gsub("\n", "")
-puts "price per ticket: "
+print "price per ticket: "
 price_per_ticket = gets.gsub("\n","")
-puts "additional ticket info: "
+print "additional ticket info: "
 additional_info = gets.gsub("\n","")
-puts "gmail email: "
+print "gmail email: "
 gmail_email = gets.gsub("\n", "")
-puts "gmail password: "
+print "gmail password: "
 gmail_password = gets.gsub("\n", "")
 
-puts "opening firefox browser..."
+print "opening firefox browser... "
 b = Watir::Browser.new :firefox
 puts "done"
 
 # Sign into ticketmaster and retrieve html
-puts "signing into ticketmaster..."
+print "signing into ticketmaster... "
 b.goto('https://www.ticketmaster.com/member?tm_link=tm_homeA_header_my_account')
 Watir::Wait.until { b.text_field(id: "email").visible? }
 b.text_field(id: "email").set(ticketmaster_email)
@@ -38,11 +40,10 @@ b.button(value: "Sign In").click
 puts "done"
 
 # Generate data for craigslist
-puts "scraping ticket data..."
+print "scraping ticket data... "
 Watir::Wait.until { b.link(text: "My Tickets").visible? }
 b.link(text: "My Tickets").click
 html = b.html
-
 event_name = html.scan(/eventData[^*]+?name":"([^*]+?)"/)[0][0]
 location = html.scan(/venueData[^*]+?name":"([^*]+?)"/)[0][0]
 date = html.scan(/date":"([^*]+?)"/)[0][0]
@@ -52,81 +53,53 @@ body = "Selling " + number_of_tickets + " " + event_name + " tickets ($" + price
 puts "done"
 
 # Post advertisement on craigslist
-puts "posting craigslist ad..."
+print "posting craigslist ad... "
 b.goto("https://post.craigslist.org/c/sfo")
-
 sleep(rand(2..5))
-
 b.radio(value: "fso").set
-
 sleep(rand(2..5))
-
 b.radio(value: "44").set
-
 sleep(rand(2..5))
-
 b.radio(value: "1").set
-
 sleep(rand(2..5))
-
 b.radios[rand(0..5)].set
-
 sleep(rand(5..20))
-
 b.text_field(name: "FromEMail").set(gmail_email)
 b.text_field(name: "ConfirmEMail").set(gmail_email)
-
 b.text_field(name: "contact_name").set(name)
-
 b.text_field(name: "PostingTitle").set(title)
-
 b.text_field(name: "Ask").set(price_per_ticket)
-
 b.text_field(name: "postal").set(zip_code)
-
 b.text_field(name: "PostingBody").set(body)
-
 b.text_field(name: "number_available").set(number_of_tickets)
-
 b.button(name: "go").click
-
 sleep(rand(2..5))
-
 b.button(text: "continue").click
-
 sleep(rand(2..5))
-
 b.button(text: "done with images").click
-
 sleep(rand(2..5))
-
 b.button(text: "publish").click
-
 puts "done"
 
 # Activating craigslist ad
-puts "activating craigslist ad..."
-
+print "activating craigslist ad... "
 b.goto("gmail.com")
 b.text_field(id: "Email").set(gmail_email)
 b.button(id:"next").click
-
 sleep(rand(2..5))
-
 b.text_field(id: "Passwd").set(gmail_password)
 b.button(id: "signIn").click
-
 sleep(rand(2..5))
-
 b.tr(class:"zA").click
 Watir::Wait.until { b.div(class: "a3s").divs[0].links[0].visible? }
 b.div(class: "a3s").divs[0].links[0].click
-
 b.window(title: "SF bay area | create posting").use  do
 	sleep(rand(2..5))
 	b.button(text: "ACCEPT the terms of use").click
 end
+puts "done"
 
+# Close browser
+print "closing browser... "
 b.close
-
 puts "done"
